ea9de34bfab7076c5b82ec743fb88f71518eea0d
Pablo Bru <pablobru08@gmail.com>
2024-10-30 22:43:13 -0300

    primera modificacion pc, models terminado, hacer funcionar test

--- src/models.ts
+++ src/models.ts
@@ -1,6 +1,7 @@
 import * as jsonfile from "jsonfile";
 // El siguiente import no se usa pero es necesario
 import "./pelis.json";
+import * as includes from "lodash"
 // de esta forma Typescript se entera que tiene que incluir
 // el .json y pasarlo a la carpeta /dist
 // si no, solo usandolo desde la libreria jsonfile, no se dá cuenta
@@ -13,11 +14,67 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  data: Peli[];
+  async getAll(): Promise<Peli[]> {
+    const datos = await jsonfile.readFile("./pelis.json")
+      this.data = datos
+      return this.data;
+  }
+  
+  add(peli:Peli):Promise<boolean>{
+    const promesaUno = this.getById(peli.id).then((peliExistente)=>{
+      if(peliExistente){
+        return false
+      }else{
+        this.data.push(peli)
+        const promesaDos = jsonfile.writeFile("./pelis.json", this.data);
+        return promesaDos.then(()=>{
+          return true
+        })
+      }
     });
+    return promesaUno
+  }
+  getById(id:number):Promise<Peli>{
+    const resultado = this.getAll().then((peli)=> {
+      return peli.find((p)=> {
+        return p.id == id;
+      })
+    })
+    return resultado
+  }
+  async search(options):Promise<Peli[]>{
+    const lista = await this.getAll()
+    const listaFiltrada = lista.filter((p)=>{
+      let esteVa = false;
+      if(options.tag){
+        return p.tags.includes(options.tag);
+        esteVa = true;
+      }
+      if(options.title){
+        return p.title.includes(options.title);
+        esteVa = true;
+      }
+      return esteVa
+    });
+    return listaFiltrada
+
   }
 }
 export { PelisCollection, Peli };
+
+const pablo = new PelisCollection()
+pablo.getAll().then((data)=>{
+  console.log(data)
+});
+pablo.getById(2).then((resultado)=>{
+  console.log(resultado)
+})
+const la = {
+  id: 5,
+  title: "holita",
+  tags: ["saludos"]
+}
+pablo.add(la).then((resultado)=> {
+  console.log(resultado)
+})
